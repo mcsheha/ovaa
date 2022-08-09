b7110bceb3057da5cad54a3ade1b0d71a9d08335
mcsheha <michael.c.shehadeh@gmail.com>
2022-08-09 18:53:27 -0400

    Fixed hardcoded keys in WeakCrypto.java

--- app/src/main/java/oversecured/ovaa/utils/WeakCrypto.java
+++ app/src/main/java/oversecured/ovaa/utils/WeakCrypto.java
@@ -6,14 +6,14 @@ import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 
 public class WeakCrypto {
-    private static final String KEY = "49u5gh249gh24985ghf429gh4ch8f23f";
+  //  private static final String KEY = "49u5gh249gh24985ghf429gh4ch8f23f";
 
     private WeakCrypto() {
     }
 
     public static String encrypt(String data) {
         try {
-            SecretKeySpec secretKeySpec = new SecretKeySpec(KEY.getBytes(), "AES");
+            SecretKeySpec secretKeySpec = new SecretKeySpec("49u5gh249gh24985ghf429gh4ch8f23f".getBytes(), "AES");
             Cipher instance = Cipher.getInstance("AES");
             instance.init(Cipher.ENCRYPT_MODE, secretKeySpec);
             return Base64.encodeToString(instance.doFinal(data.getBytes()), 0);
